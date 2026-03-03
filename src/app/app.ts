003013ec46248ba84ac9969656fd62d686a511de
FabricioLop <fabrixxlop@gmail.com>
2026-03-03 02:31:21 -0500

    feat: add footer section with contact info, social links, and CV download buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,10 @@ import { Navbar } from './components/navbar/navbar';
 import { About } from './components/about/about';
 import { Projects } from './components/projects/projects';
 import { Skills } from './components/skills/skills';
+import { Footer } from './components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,About,Projects,Skills],
+  imports: [RouterOutlet,Navbar,About,Projects,Skills,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
