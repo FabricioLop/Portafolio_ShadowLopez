03a3b6dffc1214938288493cffddab67d6c7600e
FabricioLop <fabrixxlop@gmail.com>
2026-03-03 02:19:47 -0500

    feat: add skills section with categorized tech stack

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { About } from './components/about/about';
 import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,About,Projects],
+  imports: [RouterOutlet,Navbar,About,Projects,Skills],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
