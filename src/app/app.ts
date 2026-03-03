57650656c6565c75975441e38088a8bcb08d79b1
FabricioLop <fabrixxlop@gmail.com>
2026-03-03 02:13:12 -0500

    feat: add about me section with responsive layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { About } from './components/about/about';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar],
+  imports: [RouterOutlet,Navbar,About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
